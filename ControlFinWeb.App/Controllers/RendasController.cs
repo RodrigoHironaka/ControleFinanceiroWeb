828c7d6c6fb7abdddea5154258b0fa19d0dddb1c
Rodrigo Hironaka <rocknhiro@hotmail.com>
2022-04-21 18:38:34 -0300

    Rendas CRUD concluido

---
+++ ControlFinWeb.App/Controllers/RendasController.cs
@@ -0,0 +1,79 @@
+﻿using ControlFinWeb.App.AutoMapper;
+using ControlFinWeb.App.Utilitarios;
+using ControlFinWeb.App.ViewModels;
+using ControlFinWeb.Dominio.Entidades;
+using ControlFinWeb.Repositorio.Repositorios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlFinWeb.App.Controllers
+{
+    public class RendasController : Controller
+    {
+        private readonly RepositorioRenda Repositorio;
+        public RendasController(RepositorioRenda repositorio)
+        {
+            Repositorio = repositorio;
+        }
+
+        Renda renda = new Renda();
+        RendaVM rendaVM = new RendaVM();
+
+        public IActionResult Index()
+        {
+            var rendasVM = new List<RendaVM>();
+            var rendas = Repositorio.ObterTodos();
+            foreach (Renda renda in rendas)
+                rendasVM.Add(AutoMapperConfig<Renda, RendaVM>.Mapear(renda));
+            return View(rendasVM);
+        }
+
+        public IActionResult Editar(Int64 Id = 0)
+        {
+            if (Id > 0)
+            {
+                renda = Repositorio.ObterPorId(Id);
+                rendaVM = AutoMapperConfig<Renda, RendaVM>.Mapear(renda);
+            }
+            return View(rendaVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(RendaVM rendaVM)
+        {
+            if (ModelState.IsValid)
+            {
+                if (rendaVM.Id > 0)
+                {
+                    var rendaRep = Repositorio.ObterPorId(rendaVM.Id);
+                    renda = AutoMapperConfig<RendaVM, Renda>.Mapear(rendaVM);
+                    renda.DataGeracao = rendaRep.DataGeracao;
+                    renda.UsuarioCriacao = rendaRep.UsuarioCriacao;
+                    renda.UsuarioAlteracao = Configuracao.Usuario;
+                    Repositorio.Alterar(renda);
+                }
+                else
+                {
+                    renda = AutoMapperConfig<RendaVM, Renda>.Mapear(rendaVM);
+                    renda.UsuarioCriacao = Configuracao.Usuario;
+                    Repositorio.Salvar(renda);
+                }
+                return RedirectToAction("Index");
+            }
+            return View(rendaVM);
+        }
+
+        [HttpPost]
+        public JsonResult Deletar(Int64 Id)
+        {
+            renda = Repositorio.ObterPorId(Id);
+            Repositorio.Excluir(renda);
+            return Json(renda.Nome + "excluído com sucesso");
+        }
+    }
+}
---
+++ ControlFinWeb.App/ViewModels/RendaVM.cs
@@ -0,0 +1,23 @@
+﻿using ControlFinWeb.Dominio.ObjetoValor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlFinWeb.App.ViewModels
+{
+    public class RendaVM
+    {
+        public Int64 Id { get; set; }
+
+        [DisplayName("Descrição")]
+        [StringLength(70, ErrorMessage = "Limite máximo de 70 caracteres.")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public String Nome { get; set; }
+
+        [DisplayName("Situação")]
+        public Situacao Situacao { get; set; }
+    }
+}
