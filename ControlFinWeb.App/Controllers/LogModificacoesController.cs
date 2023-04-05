9f2a4d87de49b7795fe4b57e81d4cc83dbe22a40
Rodrigo Hironaka <rocknhiro@hotmail.com>
2023-04-05 17:10:40 -0300

    View de LogModificacoes criada.

--- ControlFinWeb.App/Controllers/LogModificacoesController.cs
+++ ControlFinWeb.App/Controllers/LogModificacoesController.cs
@@ -6,6 +6,10 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using ControlFinWeb.Repositorio.Repositorios;
+using LinqKit;
+using ControlFinWeb.App.Utilitarios;
+using ControlFinWeb.App.ViewModels.Acesso;
+using System.Linq;
 
 namespace ControlFinWeb.App.Controllers
 {
@@ -17,9 +21,17 @@ namespace ControlFinWeb.App.Controllers
         {
             Repositorio = repositorio;
         }
-        public ActionResult Index()
+        public ActionResult Logs(LogModificacaoVM logModificacaoVM)
         {
-            return View();
+            var predicado = Repositorio.CriarPredicado();
+            predicado = predicado.And(x => x.UsuarioCriacao.Id == Configuracao.Usuario.Id);
+            predicado = predicado.And(x => x.DataGeracao >= logModificacaoVM.DataInicio);
+            predicado = predicado.And(x => x.DataGeracao <= logModificacaoVM.DataFinal);
+            if(!String.IsNullOrEmpty(logModificacaoVM.Chave))
+                predicado = predicado.And(x => x.Chave.Contains(logModificacaoVM.Chave));
+
+            logModificacaoVM.Logs = Repositorio.ObterPorParametros(predicado).ToList();
+            return View(logModificacaoVM);
         }
 
     }
---
+++ ControlFinWeb.App/ViewModels/Acesso/LogModificacaoVM.cs
@@ -0,0 +1,22 @@
+﻿using ControlFinWeb.Dominio.Entidades.Logs;
+using NHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+
+namespace ControlFinWeb.App.ViewModels.Acesso
+{
+    public class LogModificacaoVM
+    {
+        public LogModificacaoVM()
+        {
+            DataInicio = DateTime.Now;
+            DataFinal = DateTime.Now;
+            Logs = new List<LogModificacao>();      
+        }
+
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFinal { get; set; }
+        public String Chave { get; set; }
+        public IList<LogModificacao> Logs { get; set; }
+    }
+}
