9bf8c513bcf5701b8e51ca915871d93e83015ecd
Rodrigo Hironaka <rocknhiro@hotmail.com>
2023-04-05 21:42:27 -0300

    Ajustes layout views

--- ControlFinWeb.App/wwwroot/js/site.js
+++ ControlFinWeb.App/wwwroot/js/site.js
@@ -22,3 +22,8 @@ function ClickCheckItem() {
         }
     });
 }
+
+function HabilitarToolTip() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+}
